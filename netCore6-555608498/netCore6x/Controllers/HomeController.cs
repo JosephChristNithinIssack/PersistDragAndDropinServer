c0d9d29ed05e0bf6aefb29fe2e801edad15e4635
Joseph Christ Nithin I <69190287+JosephChristNithinIssack@users.noreply.github.com>
2023-05-22 10:34:21 +0530

    Add files via upload

---
+++ netCore6-555608498/netCore6x/Controllers/HomeController.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using netCore6x.Models;
+using System.Diagnostics;
+using Syncfusion.EJ2.Base;
+using System.Data;
+using System.Text;
+using System.Collections;
+using Newtonsoft.Json.Linq;
+
+namespace netCore6x.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public int indexcount = 0;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult UrlDatasource([FromBody] DataManagerRequest dm)
+        {
+            IEnumerable DataSource = OrdersDetails.GetAllRecords();
+            DataOperations operation = new DataOperations();
+            if (dm.Search != null && dm.Search.Count > 0)
+            {
+                DataSource = operation.PerformSearching(DataSource, dm.Search);  //Search
+            }
+            if (dm.Sorted != null && dm.Sorted.Count > 0) //Sorting
+            {
+                DataSource = operation.PerformSorting(DataSource, dm.Sorted);
+            }
+            if (dm.Where != null && dm.Where.Count > 0) //Filtering
+            {
+                DataSource = operation.PerformFiltering(DataSource, dm.Where, dm.Where[0].Operator);
+            }
+            int count = DataSource.Cast<object>().Count();
+            if (dm.Skip != 0)
+            {
+                DataSource = operation.PerformSkip(DataSource, dm.Skip);   //Paging
+            }
+            if (dm.Take != 0)
+            {
+                DataSource = operation.PerformTake(DataSource, dm.Take);
+            }
+            return dm.RequiresCounts ? Json(new { result = DataSource, count = count }) : Json(DataSource);
+        }
+
+        public void Move([FromBody] RowIndex data) {
+
+            // you can also change the position of your data based on the from and drop index 
+            if (data != null)
+            {
+
+                var tmp = OrdersDetails.GetAllRecords()[data.oldIndex];
+
+                if (data.oldIndex < data.newIndex)
+                {
+
+                    for (int i = data.oldIndex; i < data.newIndex; i++)
+                    {
+                        OrdersDetails.GetAllRecords()[i] = OrdersDetails.GetAllRecords()[i + 1];
+                    }
+                }
+                else if (data.oldIndex > data.newIndex)
+                {
+
+                    for (int i = data.oldIndex; i > data.newIndex; i--)
+                    {
+                        OrdersDetails.GetAllRecords()[i] = OrdersDetails.GetAllRecords()[i - 1];
+                    }
+                }
+
+                OrdersDetails.GetAllRecords()[data.newIndex] = tmp;
+            }
+
+
+        }
+
+
+        public class RowIndex
+        {
+            public int oldIndex { get; set; }
+            public int newIndex { get; set; }
+        }
+    }
+
+    public class OrdersDetails
+    {
+        public static List<OrdersDetails> order = new List<OrdersDetails>();
+        public OrdersDetails()
+        {
+
+        }
+        public OrdersDetails(int OrderID, string CustomerId, int EmployeeId, double Freight, bool Verified, DateTime OrderDate, string ShipCity, string ShipName, string ShipCountry, DateTime ShippedDate, string ShipAddress)
+        {
+            this.OrderID = OrderID;
+            this.CustomerID = CustomerId;
+            this.EmployeeID = EmployeeId;
+            this.Freight = Freight;
+            this.ShipCity = ShipCity;
+            this.Verified = Verified;
+            this.OrderDate = OrderDate;
+            this.ShipName = ShipName;
+            this.ShipCountry = ShipCountry;
+            this.ShippedDate = ShippedDate;
+            this.ShipAddress = ShipAddress;
+        }
+        public static List<OrdersDetails> GetAllRecords()
+        {
+            if (order.Count() == 0)
+            {
+                int code = 10000;
+                for (int i = 1; i < 5; i++)
+                {
+                    order.Add(new OrdersDetails(code + 1, "ALFKI", i + 0, 2.3 * i, false, new DateTime(1991, 05, 15), "Berlin", "Simons bistro", "Denmark", new DateTime(1996, 7, 16), "Kirchgasse 6"));
+                    order.Add(new OrdersDetails(code + 2, "ANATR", i + 2, 3.3 * i, true, new DateTime(1990, 04, 04), "Madrid", "Queen Cozinha", "Brazil", new DateTime(1996, 9, 11), "Avda. Azteca 123"));
+                    order.Add(new OrdersDetails(code + 3, "ANTON", i + 1, 4.3 * i, true, new DateTime(1957, 11, 30), "Cholchester", "Frankenversand", "Germany", new DateTime(1996, 10, 7), "Carrera 52 con Ave. BolĂ­var #65-98 Llano Largo"));
+                    order.Add(new OrdersDetails(code + 4, "BLONP", i + 3, 5.3 * i, false, new DateTime(1930, 10, 22), "Marseille", "Ernst Handel", "Austria", new DateTime(1996, 12, 30), "Magazinweg 7"));
+                    order.Add(new OrdersDetails(code + 5, "BOLID", i + 4, 6.3 * i, true, new DateTime(1953, 02, 18), "Tsawassen", "Hanari Carnes", "Switzerland", new DateTime(1997, 12, 3), "1029 - 12th Ave. S."));
+                    code += 5;
+                }
+            }
+            return order;
+        }
+
+        public int? OrderID { get; set; }
+        public string CustomerID { get; set; }
+        public int? EmployeeID { get; set; }
+        public double? Freight { get; set; }
+        public string ShipCity { get; set; }
+        public bool Verified { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        public string ShipName { get; set; }
+
+        public string ShipCountry { get; set; }
+
+        public DateTime ShippedDate { get; set; }
+        public string ShipAddress { get; set; }
+    }
+}
